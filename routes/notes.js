b4b2485bb8a578c16794b0b9e0ae0b11ff8750ea
Kirandeep <kirandeep1195@gmail.com>
2023-07-18 21:30:10 -0400

    modified nots file

--- routes/notes.js
+++ routes/notes.js
@@ -2,15 +2,14 @@ const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
 const fs = require('fs');
 
-
 // http://localhost:3001/api/notes/
-//GET /api/notes  read the db.json file and return all saved notes as JSON.
+//GET /api/notes read the db.json file and return all saved notes as JSON.
 notes.get('/', (req, res) => {
   console.info(`${req.method} note maker`);
   fs.readFile('./db/db.json', 'utf8', function (error, data) { res.json(JSON.parse(data)) });
 })
 
-//POST /api/notes receive a new note to save on the request body, add it to the db.json file,
+//POST /api/notes  receive a new note to save on the request body, add it to the db.json file,
 // and then return the new note to the client. 
 
 notes.post('/', (req, res) => {
@@ -45,8 +44,6 @@ notes.post('/', (req, res) => {
     );
   })
 })
-
-
 
 
 
